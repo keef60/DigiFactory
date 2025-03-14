68f7a36106f4af6a781a8448a3fb013b4ad2e884
keef60 <keef60@gmail.com>
2025-03-14 17:01:45 -0500

    Update lookupPane.js

--- lookupPane.js
+++ lookupPane.js
@@ -1,46 +1,61 @@
 const { useState, useEffect } = React;
 
 const LookupComponent = ({
-    filteredData, 
-    headers, 
-    imagePaths, 
-    getPdfPath, 
-    openPdfModal, 
-    openNoteModal, 
-    setPdfPath, 
-    setPdfPath2, 
-    setPdfPath3, 
-    setNotePath, 
-    setWorkingThisRow, 
-    setGoal, 
-    setProgress, 
-    goal, 
-    progress, 
-    workingThisRow, 
-    calculateCompletion, 
+    filteredData,
+    headers,
+    imagePaths,
+    getPdfPath,
+    openPdfModal,
+    openNoteModal,
+    setPdfPath,
+    setPdfPath2,
+    setPdfPath3,
+    setNotePath,
+    setWorkingThisRow,
+    setGoal,
+    setProgress,
+    goal,
+    progress,
+    workingThisRow,
+    calculateCompletion,
     calculateRemaining,
     goalProgressInput,
-    departmentName
+    departmentName,
+    lookuptable,
+    spMethod,
+    issue,
+    lineSelection,
+    selectedNumber,
+    setSelectedNumber,
+    chart
 }) => {
-console.log('lookup Pane',departmentName);
-    return React.createElement('div', { className: 'ui divided items sixteen wide column ' }, 
-        filteredData.slice().map((row, rowIndex) => 
-            React.createElement('div', { key: rowIndex, className: 'ui segment basic' }, 
-                React.createElement('div', { className: 'ui divider' }), 
-                React.createElement('h1', { className: 'ui header' }, 
-                    React.createElement('div', { className: 'ui label' }, `${headers[0]} ${row[0] || 'Unnamed'}`)
-                ),
-                React.createElement('div', { className: 'ui grid ' },
-                    React.createElement('div', { className: 'four wide column', style: { textAlign: 'center' } }, 
-                        imagePaths[row[0]] && imagePaths[row[0]] !== 'img/default_image.jpg' ? 
-                            React.createElement('img', { className: 'ui fluid image', src: imagePaths[row[0]], alt: 'Loaded Image' }) : 
-                            React.createElement('div', { className: 'ui placeholder' }, 
+    console.log('lookup Pane', departmentName);
+
+
+    const departmentRefName = departmentName.charAt(0).toUpperCase() + departmentName.slice(1);
+
+    return React.createElement('div', { className: 'ui divided items sixteen wide column ' },
+
+        filteredData.slice(0).map((row, rowIndex) =>
+            React.createElement('div', { key: rowIndex, className: `ui segment basic ` },
+                React.createElement('div', { className: 'ui divider' }),
+                React.createElement('div', { className: 'ui divider hidden ' }),
+                departmentName === 'line' && React.createElement(
+                    lineSelection, {
+                    selectedNumber,
+                    setSelectedNumber,
+                }),
+                React.createElement('div', { className: 'ui grid internally celled ' },
+                    React.createElement('div', { className: 'four wide column', style: { textAlign: 'center' } },
+                        imagePaths[row[0]] && imagePaths[row[0]] !== 'img/default_image.jpg' ?
+                            React.createElement('img', { className: 'ui fluid image', src: imagePaths[row[0]], alt: 'Loaded Image' }) :
+                            React.createElement('div', { className: 'ui placeholder' },
                                 React.createElement('div', { className: 'image' })
                             ),
-                        React.createElement('div', { className: 'ui divider hidden' }),
-                        React.createElement('div', { className: 'ui buttons' }, 
+                        React.createElement('div', { className: 'ui divider hidden ' }),
+                        React.createElement('div', { className: 'ui buttons  ' },
                             React.createElement('button', {
-                                className: 'ui blue small button',
+                                className: 'ui black small button',
                                 onClick: async () => {
                                     const [pdf, pdf2, pdf3] = getPdfPath(row);
                                     setPdfPath(pdf);
@@ -54,57 +69,66 @@ console.log('lookup Pane',departmentName);
                                 }
                             }, 'Drawing'),
                             React.createElement('button', {
-                                className: 'ui blue small button',
+                                className: 'ui  small button',
                                 onClick: () => {
                                     setNotePath(row[0]);
                                     openNoteModal();
                                 }
                             }, 'Notes')
+                        ),
+
+
+                    ),
+
+                    React.createElement(chart, {
+                        columnSize: 'six',
+                        headers,
+                        row,
+                        departmentName,
+                        selectedNumber,
+                        modelId:row[0]
+                    }),
+
+                    React.createElement('div', { className: ' ui  sixteen wide column row', style: { padding: "2.5%" } },
+                        React.createElement('div', { className: ' ui header huge ' }, `Performance and Goal Monitoring Dashboard`,
+                            React.createElement('div', { className: ' ui sub header' }, `${departmentRefName}`)
                         )
                     ),
 
+                    // New section for Goal and Progress input
+                    React.createElement(goalProgressInput, {
+                        row,
+                        workingThisRow,
+                        goal,
+                        progress,
+                        setWorkingThisRow,
+                        setGoal,
+                        setProgress,
+                        calculateCompletion,
+                        calculateRemaining,
+                        departmentName,
+                        spMethod,
+                        selectedNumber,
+                        issue
+                    }),
 
+                    //Header Goal and Progress 
+                    React.createElement('div', { className: ' ui  sixteen wide column row', style: { padding: '2.5%' } },
 
- // New section for Goal and Progress input
- React.createElement(goalProgressInput,{
-    row,
-    workingThisRow,
-    goal,
-    progress,
-    setWorkingThisRow,
-    setGoal,
-    setProgress,
-    calculateCompletion,
-    calculateRemaining,
-    departmentName }),
+                        React.createElement('div', { className: ' ui header huge ' }, `Manufacturing Issues and Action Dashboard`,
+                            React.createElement('div', { className: ' ui sub header' }, `${departmentRefName}`)
+                        )),
 
-                    React.createElement('div', { className: 'sixteen wide column' },
-                        React.createElement('table', { className: 'ui celled striped table' },
-                            React.createElement('tbody', null,
-                                // First row - Active Indicator if progress > 0
-                                calculateCompletion(
-                                    workingThisRow === row[0] ? goal : JSON.parse(localStorage.getItem(`goalProgress-paint-${row[0]}`))?.goal,
-                                    workingThisRow === row[0] ? progress : JSON.parse(localStorage.getItem(`goalProgress-paint-${row[0]}`))?.progress
-                                ) > 0 &&
-                                React.createElement('tr', null,
-                                    React.createElement('td', { colSpan: 2, style: { textAlign: 'center' } },
-                                        React.createElement('div', { className: 'ui label green', style: { display: 'flex', alignItems: 'center', fontWeight: 'bold' } },
-                                            React.createElement('div', { className: 'dot', style: { width: '10px', height: '10px', backgroundColor: 'green', borderRadius: '50%', marginRight: '5px' } }),
-                                            'Active'
-                                        )
-                                    )
-                                ),
+                    React.createElement(issue, {
+                        spMethod,
+                        departmentName,
+                        modelId: row[0]
+                    }),
 
-                                // Other rows from the headers
-                                headers.slice().map((header, colIndex) =>
-                                    React.createElement('tr', { key: colIndex },
-                                        React.createElement('td', { style: { fontWeight: 'bold' } }, header || 'Field'),
-                                        React.createElement('td', null, row[colIndex] || 'N/A')
-                                    )
-                                )
-                            )
-                        )
-                    )
+                    departmentName === 'paint' && React.createElement(lookuptable, {
+                        headers,
+                        row
+                    })
                 )
             )
         )
