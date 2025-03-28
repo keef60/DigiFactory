294d32a1f90e19b37d217d4f079dc76c6d4c22d1
keef60 <keef60@gmail.com>
2025-03-28 16:53:47 -0500

    standalone

---
+++ app.js
@@ -0,0 +1,304 @@
+const { useState, useEffect } = React
+function DepartmentMenu() {
+
+    const [selectedDepartment, setSelectedDepartment] = useState('Paint');
+    const [departmentIcon, setDepartmentIcon] = useState('certificate');
+    // State for handling search query
+    const [searchQueryLifted, setSearchQuery] = useState('');
+    // State for controlling the visibility of the save warning message
+    const [visibleLifted, setVisible] = useState(false);
+    const [dataLifted, setData] = useState([]);
+    const [sheetNameLifted, setSheetName] = useState('');
+    const [tableData, setTableData] = useState(null);
+    const [packoutTableData, setPackoutTableData] = useState(null);
+    const [tableHeaders, setHeaders] = useState([
+        ["Model", "Description", "All of packout kits", "Oil", "Gun", "Lance", "Soap Hose / Filter", "knob bolts", "Hose", "Hose Hanger", "Gun Holder"],
+        ['Model #', 'Frame Color', 'Raw Frame', 'Frame #', 'Frame Description', 'Raw Handle', 'Handle', 'Handle Description', 'Raw 2nd Handle', '2nd Handle', '2nd Handle Description']
+    ])
+    // State to store the selected number
+    const [selectedNumber, setSelectedNumber] = useState(null);
+    const [loginModalOpen, setLoginModalOpen] = useState(false); // State to control login modal visibility
+    const [clearLoading, setClearLoading] = useState(true);
+    const [userInfo, setUserInfo] = useState(undefined);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [woNdev, setWOnDev] = useState();
+    const [userName, setUserName] = useState(undefined);
+    const [newError, setError] = useState(undefined);
+
+
+    useEffect(() => {
+        if (!tableData) {
+
+            main.fetchSharePointData('FRAMETABLE', 'all', true, setTableData, '')
+                .then(e => e)
+                .catch(err => console.log(err.error.message))
+
+            main.fetchSharePointData('PACKOUTTABLE', 'all', true, '', setPackoutTableData)
+                .then(e => e)
+                .catch(err => console.log(err.error.message));
+
+
+        } else if (dataLifted.length === 0 && Array.isArray(tableData) && selectedDepartment !== 'Packout') {
+
+            let table = [tableHeaders[1]];
+            let arry = [];
+            for (let t of tableData) {
+                arry = [t.fields.Title];
+                for (let i = 1; i <= 10; i++) {
+                    arry.push(t.fields[`field_${i}`]);
+                }
+                table.push(arry);
+            }
+            setData(table);
+            setClearLoading(false);
+
+
+        } else if (dataLifted.length === 0 && Array.isArray(packoutTableData) && selectedDepartment === 'Packout') {
+
+            let table = [tableHeaders[0]];
+            let arry = [];
+            for (let t of packoutTableData) {
+                arry = [t.fields.Title];
+                for (let i = 2; i <= 9; i++) {
+                    arry.push(t.fields[`field_${i}`]);
+                }
+                table.push(arry);
+            }
+            setData(table);
+            setClearLoading(false);
+        }
+
+
+    }, [tableData, packoutTableData, selectedDepartment]);
+
+    useEffect(() => {
+        $('.ui.login.dimmer').dimmer('hide');
+        getMe();
+    }, [userInfo, isLoggedIn, newError]);
+
+    useEffect(() => {
+        if (userInfo) {
+            setUserName(userInfo.displayName);
+        }
+    }, [userInfo]);
+
+    const getMe = async () => {
+        const accessToken = sessionStorage.getItem('access_token');
+        try {
+            if (!userInfo && accessToken) {
+                fetch("https://graph.microsoft.com/v1.0/me", {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`
+                    }
+                })
+                    .then(response => response.json())
+                    .then(data => { setUserInfo(data); setIsLoggedIn(true); })
+                    .catch(error => {
+
+                        setIsLoggedIn(false);
+                        setError(error);
+                        console.log('======================>', error)
+                    });
+            }
+        } catch (error) {
+
+            setIsLoggedIn(false);
+            setError(error);
+            console.log('<========================>', error)
+        }
+
+    }
+
+
+    const handleDepartmentClick = (department) => {
+        setData([]);
+        setSelectedDepartment(department);
+    };
+
+    const contentMasterSeletor = (selectedDepartmentListName, departmentName) => {
+        return (
+            <LinesEditorNew
+                spMethod={main}
+                selectedDepartment={selectedDepartmentListName}
+                departmentName={departmentName}
+                searchQueryLifted={searchQueryLifted}
+                visibleLifted={visibleLifted}
+                dataLifted={dataLifted}
+                sheetNameLifted={sheetNameLifted}
+                setSelectedNumber={setSelectedNumber}
+                selectedNumber={selectedNumber}
+                clearLoading={clearLoading}
+                setWOnDev={setWOnDev}
+                woNdev={woNdev}
+            />
+        )
+    }
+
+
+
+
+
+    const renderContent = () => {
+        switch (selectedDepartment) {
+            case 'Paint':
+                return (
+                    contentMasterSeletor("FRAMES KIT", 'frames')
+                )
+            case 'Packout':
+                return (
+                    contentMasterSeletor("PACKOUT KIT", 'packout')
+                )
+            case 'Handles':
+                return (
+                    contentMasterSeletor("HANDLE KIT", 'handles')
+                )
+            case 'Frames':
+                return (
+                    contentMasterSeletor("FRAME KIT", 'frames')
+                )
+            case 'Lines':
+                return (
+                    contentMasterSeletor("LINES KIT", 'line')
+                )
+            case 'Inventory':
+                return (
+
+                    <InventoryLookup
+                        spMethod={main}
+                        selectedDepartment="Inventory"
+                        departmentName={['inventory', 'sage']}
+                        searchQueryLifted={searchQueryLifted}
+                        clearLoading={clearLoading}
+                    />
+
+                );
+            case 'Inventory Levels':
+                return (
+
+                    <InventoryLookup
+                        spMethod={main}
+                        selectedDepartment="Inventory"
+                        departmentName={['inventory', 'sage']}
+                        searchQueryLifted={searchQueryLifted}
+                        clearLoading={clearLoading}
+                    />
+
+                );
+
+            case 'Daily Production Overview': return (
+                <DailyProductionOverview />
+            );
+            case 'Inventory Levels': return (
+                <InventoryLevels />
+            );
+            case 'Maintenance Status': return (
+                <MaintenanceStatus />
+            );
+            case 'Throughput Report': return (
+                <ThroughputReport />
+            );
+            case 'Yield Analysis': return (
+                <YieldAnalysis />
+            );
+            case 'Production Downtime Report': return (
+                <ProductionDowntimeReport spMethod={main} />
+            );
+            case 'Real-Time Production Report':
+                return (
+                    <div className="ui">
+                        <Dashboard spMethod={main} />
+                    </div>
+                );
+
+            default:
+                return (
+                    <div className="ui sixteen wide column segment" style={{ position: 'absolute', top: '60%', left: '50%', transform: 'translate(-50%, 60%)' }}>
+                        <h2>No Access Yet</h2>
+                        <p>
+                            Please contact admin for access to this department.
+
+                        </p>
+                    </div>
+                );
+        }
+    };
+
+    const header = () => {
+        return (
+            <div>
+                <h1 className="ui header huge" style={{ marginTop: '2%', marginLeft: '3%' }}>
+                    <div className="ui image avatar">
+                        <img
+                            src="img/logo.jpg" // replace with your image source
+                            alt="Department Image"
+                        />
+                    </div>
+                    {selectedDepartment}
+                </h1>
+            </div>
+        );
+    };
+
+    const loginUser = () => {
+
+        setIsLoggedIn(true);
+        setUserName(userInfo.displayName);
+
+    }
+    const loginModal = () => {
+        return (
+            <div className={`ui page dimmer login ${loginModalOpen ? 'active show' : 'hide'}`}>
+                <div className="header">Login</div>
+                <div className="content">
+                    <LoginTokenNew
+                        setIsLoggedIn={loginUser}
+                        isLoggedIn={isLoggedIn}
+                    />
+                </div>
+                <div className="actions">
+                    <button
+                        className="ui red button deny"
+                        onClick={() => setLoginModalOpen(false)}
+                    >
+                        Close
+                    </button>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div className="ui">
+            <div className="ui grid contentPane">
+                {/* Left Sidebar Menu */}
+                <LeftMenuBar
+                    handleDepartmentClick={handleDepartmentClick}
+                    setClearLoading={setClearLoading}
+                    setLoginModalOpen={setLoginModalOpen}
+                    searchQueryLifted={searchQueryLifted}
+                    setSearchQuery={setSearchQuery}
+                    visibleLifted={visibleLifted}
+                    setVisible={setVisible}
+                    setData={setData}
+                    dataLifted={dataLifted}
+                    sheetNameLifted={sheetNameLifted}
+                    setSheetName={setSheetName}
+                    userName={userName}
+                    selectedDepartment={selectedDepartment}
+                    loginModalOpen={loginModalOpen}
+                />
+
+                {/* Content Area with manually created tabs */}
+                <div className="ui sixteen wide column" style={{ marginLeft: '15.5%', paddingRight: '5%' }}>
+                    <ErrorMessage error={newError} />
+                    {header()}
+                    {renderContent()}
+                    {loginModal()}
+                </div>
+            </div>
+        </div>
+    );
+}
+ReactDOM.render(<DepartmentMenu />, document.getElementById('root'));
+
--- app2 Update.js
+++ app2 Update.js
@@ -12,13 +12,19 @@ import PackoutLookup from './components/lookupPanePackout.js';
 import TopMenuBar from './components/searchBar.js';
 import AddRecordModal from './components/newRecordModal.js';
 import DetailPane from './components/detailPane.js';
+import DetailPaneMini from './components/detailPaneMini.js';
 import LookUpTable from './components/lookupTable.js';
 import SkillSelect from './components/issesPane.js';
 import LinesEditor from './js/line.js';
 import LineSelection from './components/chooseLine.js';
 import ChartContainer from './components/chart.js';
+import InventoryLookup from './js/inventory_Editor.js';
+import LoginToken from './js/login.js';
+import Dashboard from './components/dashboard.js';
+
 function DepartmentMenu() {
-  const [selectedDepartment, setSelectedDepartment] = useState('Paint');
+
+  const [selectedDepartment, setSelectedDepartment] = useState('Reports');
   const [departmentIcon, setDepartmentIcon] = useState('certificate');
   // State for handling search query
   const [searchQueryLifted, setSearchQuery] = useState('');
@@ -29,30 +35,33 @@ function DepartmentMenu() {
   const [tableData, setTableData] = useState(null);
   const [packoutTableData, setPackoutTableData] = useState(null);
   const [tableHeaders, setHeaders] = useState([
-    ["Model",	"Description", 	"All of packout kits","Oil","Gun",	"Lance",	"Soap Hose / Filter",	"knob bolts","Hose",	"Hose Hanger",	"Gun Holder"],
+    ["Model", "Description", "All of packout kits", "Oil", "Gun", "Lance", "Soap Hose / Filter", "knob bolts", "Hose", "Hose Hanger", "Gun Holder"],
     ['Model #', 'Frame Color', 'Raw Frame', 'Frame #', 'Frame Description', 'Raw Handle', 'Handle', 'Handle Description', 'Raw 2nd Handle', '2nd Handle', '2nd Handle Description']
   ])
-  const [generalFrameData, setGeneralFrameData] = useState([]);
-   // State to store the selected number
-   const [selectedNumber, setSelectedNumber] = useState(null);
+  const [generalFrameData, setGeneralFrameData] = useState();
+  // State to store the selected number
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const [loginModalOpen, setLoginModalOpen] = useState(false); // State to control login modal visibility
+  const [clearLoading, setClearLoading] = useState(true);
+  const [ userInfo,setUserInfo] = useState();
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const [woNdev,setWOnDev] = useState();
+  const [userName, setUserName] = useState(undefined);
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [headers] = useState(['Name', 'Age', 'Email']);  // Example headers
-  const [records, setRecords] = useState([]);
 
   useEffect(() => {
     if (!tableData) {
 
-        main.fetchSharePointData('FRAMETABLE', 'all', true, setTableData,'')
-          .then(e => e)
-          .catch(err => console.log(err.error.message))
-        
-        main.fetchSharePointData('PACKOUTTABLE', 'all', true, '',setPackoutTableData)
-          .then(e =>e)
-          .catch(err => console.log(err.error.message));
+      main.fetchSharePointData('FRAMETABLE', 'all', true, setTableData, '')
+        .then(e => e)
+        .catch(err => console.log(err.error.message))
+
+      main.fetchSharePointData('PACKOUTTABLE', 'all', true, '', setPackoutTableData)
+        .then(e => e)
+        .catch(err => console.log(err.error.message));
 
 
-    } else if (Array.isArray(tableData) && selectedDepartment !=='Packout') {
+    } else if (dataLifted.length === 0 && Array.isArray(tableData) && selectedDepartment !== 'Packout') {
 
       let table = [tableHeaders[1]];
       let arry = [];
@@ -64,7 +73,10 @@ function DepartmentMenu() {
         table.push(arry)
       }
       setData(table);
-    } else if (Array.isArray(packoutTableData) && selectedDepartment ==='Packout') {
+      setClearLoading(false);
+
+
+    } else if (dataLifted.length === 0 && Array.isArray(packoutTableData) && selectedDepartment === 'Packout') {
 
       let table = [tableHeaders[0]];
       let arry = [];
@@ -76,47 +88,15 @@ function DepartmentMenu() {
         table.push(arry)
       }
       setData(table);
+      setClearLoading(false);
 
     }
 
 
-  }, [tableData, packoutTableData,selectedDepartment])
-
-
- 
-
-  // Handle adding a new record
-  const handleAddRecord = (newRecord) => {
-    setRecords([...records, newRecord]);
-
-  };
-
-  // Toggle modal visibility
-  const toggleModal = () => {
-    setIsModalOpen(!isModalOpen);
-  };
-
-
-  const handleDepartmentClick = (department) => {
-    setSelectedDepartment(department);
-  };
-
-  const saveFile = () => {
-
-    try {
-      let ws = XLSX.utils.aoa_to_sheet(dataLifted);
-      let wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, sheetNameLifted);
-      XLSX.writeFile(wb, 'Paint_Open_This_Lastest_File.xlsx');
-    } catch (error) {
-      console.error('Error saving file:', error);
-      alert('Failed to save file.');
-    }
-    setVisible(false);
-
-  };
+  }, [tableData, packoutTableData, selectedDepartment])
 
   useEffect(() => {
+
     if (selectedDepartment === 'Paint') {
       setDepartmentIcon('certificate')
     } else if (selectedDepartment === 'Packout') {
@@ -125,25 +105,57 @@ function DepartmentMenu() {
       setDepartmentIcon('certificate')
     } else if (selectedDepartment === 'Frames') {
       setDepartmentIcon('certificate');
-    }else if (selectedDepartment === 'Lines') {
+    } else if (selectedDepartment === 'Lines') {
       setDepartmentIcon('cog');
     }
 
-  }, [selectedDepartment])
+  }, [selectedDepartment,isLoggedIn])
+
+  useEffect(() => {
+    $('.ui.login.dimmer').dimmer()
+    getMe()
+    setUserName(userInfo?.displayName)
+  },[userInfo,isLoggedIn])
+
+  const getMe = async()=>{
+
+     const accessToken = sessionStorage.getItem('access_token');
+    try {
+      if(!userInfo)
+       fetch("https://graph.microsoft.com/v1.0/me", {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${accessToken}`
+        }
+      })
+      .then(response => response.json())
+      .then(data => {setUserInfo(data);setIsLoggedIn(true);})
+      .catch(error => setIsLoggedIn(false));
+    } catch (error) {
+      setIsLoggedIn(false);
+    }
+   
+      
+}
+
+  const handleDepartmentClick = (department) => {
+    setData([])
+    setSelectedDepartment(department);
+  };
 
   const renderContent = () => {
     if (selectedDepartment === 'Paint') {
 
-      return React.createElement('div', { className: 'ui active' },
+      return React.createElement('div', { className: 'ui ' },
 
         React.createElement(PaintEditor, {
           spMethod: main,
           pickListApp: PickListApp,
-          selectedDepartment: "PAINT KIT",
+          selectedDepartment: "FRAMES KIT",
           displayPane: DisplayPane,
           lookupComponent: LookupComponent,
           goalProgressInput: DetailPane,
-          departmentName: 'paint',
+          departmentName: 'frames',
           searchQueryLifted,
           visibleLifted,
           dataLifted,
@@ -151,13 +163,17 @@ function DepartmentMenu() {
           setData,
           lookuptable: LookUpTable,
           issue: SkillSelect,
-          lineSelection:LineSelection,
-          chart:ChartContainer
+          lineSelection: LineSelection,
+          chart: ChartContainer,
+          clearLoading,
+          detailPaneMini:DetailPaneMini,
+          setWOnDev,
+          woNdev
 
         })
       );
     } else if (selectedDepartment === 'Packout') {
-      return React.createElement('div', { className: 'ui active' },
+      return React.createElement('div', { className: 'ui ' },
 
         React.createElement(PackOutEditor, {
           spMethod: main,
@@ -174,13 +190,18 @@ function DepartmentMenu() {
           setData,
           lookuptable: LookUpTable,
           issue: SkillSelect,
-          lineSelection:LineSelection,
-          chart:ChartContainer
+          lineSelection: LineSelection,
+          chart: ChartContainer,
+          clearLoading,
+          detailPaneMini:DetailPaneMini,
+          setWOnDev,
+          woNdev
+
 
         })
       );
     } else if (selectedDepartment === 'Handles') {
-      return React.createElement('div', { className: 'ui active' },
+      return React.createElement('div', { className: 'ui ' },
 
         React.createElement(HandlesEditor, {
           spMethod: main,
@@ -197,14 +218,19 @@ function DepartmentMenu() {
           setData,
           lookuptable: LookUpTable,
           issue: SkillSelect,
-          lineSelection:LineSelection,
-          chart:ChartContainer
+          lineSelection: LineSelection,
+          chart: ChartContainer,
+          clearLoading,
+          detailPaneMini:DetailPaneMini,
+          setWOnDev,
+          woNdev
+
 
 
         })
       );
     } else if (selectedDepartment === 'Frames') {
-      return React.createElement('div', { className: 'ui active' },
+      return React.createElement('div', { className: 'ui ' },
 
         React.createElement(FramesEditor, {
           spMethod: main,
@@ -221,13 +247,18 @@ function DepartmentMenu() {
           setData,
           lookuptable: LookUpTable,
           issue: SkillSelect,
-          lineSelection:LineSelection,
-          chart:ChartContainer
+          lineSelection: LineSelection,
+          chart: ChartContainer,
+          clearLoading,
+          detailPaneMini:DetailPaneMini,
+          setWOnDev,
+          woNdev
+
 
         })
       );
     } else if (selectedDepartment === 'Lines') {
-      return React.createElement('div', { className: 'ui active' },
+      return React.createElement('div', { className: 'ui ' },
 
         React.createElement(LinesEditor, {
           spMethod: main,
@@ -244,34 +275,53 @@ function DepartmentMenu() {
           setData,
           lookuptable: LookUpTable,
           issue: SkillSelect,
-          lineSelection:LineSelection,
-          selectedNumber, 
+          lineSelection: LineSelection,
+          selectedNumber,
           setSelectedNumber,
-          chart:ChartContainer
+          chart: ChartContainer,
+          clearLoading,
+          detailPaneMini:DetailPaneMini,
+          setWOnDev,
+          woNdev
+
 
         })
       );
+    } else if (selectedDepartment === 'Inventory') {
+      return React.createElement('div', { className: 'ui ' },
+        React.createElement(InventoryLookup, {
+          spMethod: main,
+          selectedDepartment: "Inventory",
+          departmentName: ['inventory', 'sage'],
+          searchQueryLifted,
+          clearLoading,
+        }))
+    } else if (selectedDepartment === 'Reports') {
+      return React.createElement('div', { className: 'ui ' },
+        React.createElement(Dashboard, {spMethod:main})
+      )
     } else {
-      return React.createElement('div', { className: 'ui container' },
+      return React.createElement('div', { className: 'ui sixteen wide column segment ', style: {
+        position: 'absolute',
+        top: '60%',
+        left: '50%',
+        transform: 'translate(-50%, 60%)'
+      }
+       },
         React.createElement('h2', null, 'No Access Yet'),
-        React.createElement('p', null, 'Please contact admin for access to this department.')
+        React.createElement(
+          'p',
+          null,
+          'Please contact admin for access to this department. ',
+          React.createElement(
+            'a',
+            { href: `mailto:${userInfo.mail}` },
+            userInfo.mail
+          )
+        )
+        
       );
     }
-  };
-
-  // Render the tabs manually
-  const renderTabs = () => {
-    const departments = ['Paint', 'Packout', 'Handles', 'Pumps', 'Hose', 'Frames'];
-
-    return React.createElement('div', { className: 'ui top attached tabular menu', style: { position: 'sticky', top: 0, zIndex: 1000, background: 'white' } },
-      departments.map(department =>
-        React.createElement('a', {
-          key: department,
-          className: `item ${selectedDepartment === department ? 'active' : ''}`,
-          onClick: () => setSelectedDepartment(department)
-        }, department)
-      )
-    );
   };
 
   const topBar = () => {
@@ -288,59 +338,82 @@ function DepartmentMenu() {
     })
   };
 
-  /* const newRecordModal = ()=>{
-    return React.createElement(AddRecordModal,{ 
-      isOpen, 
-      headers, 
-      onClose, 
-      onAddRecord, 
-      selectedDepartment
-     })
-  } */
-
-
-
   const header = () => {
-    return React.createElement('div', null,
-      React.createElement('h2', { className: 'ui header', style: { marginTop: '20px' } },
-        React.createElement('i', { className: 'large icons' },
-          React.createElement('i', { className: `${departmentIcon} icon blue mini` })
-        ),
-        `${selectedDepartment} Department`,
-        /*       React.createElement('div', { className: 'sub header ui' },"Centralized Automated Real-Time Reporting: Streamlining Efficiency and Data-Driven Decisions")
-         */
-      ))
+   return React.createElement('div', null,
+      
+      React.createElement('h1', { className: 'ui header huge', style: { marginTop: '2%', marginLeft: '3%' } },
+        React.createElement('div',{className:'ui  image avatar'},
+          React.createElement('img', { 
+            className:'',
+            src: 'img/logo.jpg', // replace with your image source
+            alt: 'Department Image',
+          })
+        ), `${selectedDepartment}`
+      )
+    )
+    
   };
 
   const leftMenuBar = () => {
-    return React.createElement('div', { className: 'ui left fixed vertical menu  ', style: { height: '100%' } },
+    return React.createElement('div', { className: 'ui left fixed vertical menu   ',/*  style: { margin: ".75%" } */ },
       React.createElement('div', { className: 'item' },
-        React.createElement('img', { className: 'ui avatar image', src: 'img/logo.jpg', alt: 'Logo' }),
-        React.createElement('span', {}, 'FNA Dashboard')
+        React.createElement('div', { className: 'ui header' },`${userName?userName:'Not Logged In'}`),
       ),
-      ['Paint', 'Handles', 'Pumps', 'Packout', 'Hose', 'Frames','Lines'].map(department =>
+      topBar(),
+      ['Reports', 'Paint', 'Handles', 'Pumps', 'Packout', 'Hose', 'Frames', 'Lines', 'Inventory'].map(department =>
         React.createElement('a', {
           key: department,
           className: `item ${selectedDepartment === department ? 'active' : ''}`,
-          onClick: () => handleDepartmentClick(department)
+          onClick: () => { handleDepartmentClick(department); setClearLoading(true); }
         }, department)
-      )
+      ), // Login button to toggle the login modal
+      React.createElement('a', {
+        className: ' item ',       
+
+      }, React.createElement('button', { className: `ui ${userName?'red':'primary'} button fluid`, onClick: () =>{ setLoginModalOpen(!loginModalOpen)}, }, `${userName?'Logout':'Login'}`))
+
     )
   };
 
+  const loginUser = ()=>{
+
+    setIsLoggedIn(true);
+    setSelectedDepartment('Paint');
+    setUserName(userInfo.displayName);
+
+  }
+  // Render the login modal using className
+  const loginModal = () => {
+    return React.createElement('div', {
+      className: `ui page dimmer login ${loginModalOpen ? 'active' : ''}`, // React controls visibility
+    },
+      React.createElement('div', { className: 'header' }, 'Login'),
+      React.createElement('div', { className: 'content' },
+        React.createElement(LoginToken, {setIsLoggedIn:loginUser,isLoggedIn:isLoggedIn})
+      ),
+      React.createElement('div', { className: 'actions' },
+        React.createElement('button', {
+          className: 'ui red button deny',
+          onClick: () => setLoginModalOpen(false)
+        }, 'Close')
+      )
+    );
+  };
+
   return React.createElement('div', { className: 'ui  ' },
-    topBar(),
-    React.createElement('div', { className: 'ui grid container ' },
+    React.createElement('div', {
+      className: 'ui grid  contentPane '
+    },
       // Left Sidebar Menu
       leftMenuBar(),
-
       // Content Area with manually created tabs
-      React.createElement('div', { className: 'ui sixteen wide column', style: { marginLeft: '15%', "padding-right": '15%', } },
-        /* newRecordModal(), */
-
+      React.createElement('div', {
+        className: 'ui sixteen wide column',
+        style: { marginLeft: '15.5%', "padding-right": '5%', }
+      },
         header(),
-
         renderContent(),
+        loginModal()
 
       )
     ));
