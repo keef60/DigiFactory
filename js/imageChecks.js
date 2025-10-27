d02766d565550aa7494fa557bcac1008bf3ab9af
keef60 <keef60@gmail.com>
2025-10-27 15:03:17 -0500

    Delete js directory

--- js/imageChecks.js
+++
@@ -1,9 +0,0 @@
-const calculateCompletion = (goal, progress) => {
-    if (!goal || !progress) return 0;
-    return Math.min((progress / goal) * 100, 100);
-  };
-
-  const calculateRemaining = (goal, progress) => {
-    if (!goal || !progress) return 0;
-    return goal - progress;
-  };
